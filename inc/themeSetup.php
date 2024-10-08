b99e2041b154c853d3e609efc8faea3568b2c8a3
venuvivek-cheruku <venuvivek.cheruku@outlook.com>
2024-10-09 00:20:26 +0100

    added single product page

--- inc/themeSetup.php
+++ inc/themeSetup.php
@@ -40,3 +40,44 @@
         echo '</p>';
     }
     add_action('woocommerce_login_form', 'add_lost_password_above_button', 15);
+
+    // Remove product tabs from the single product page
+    remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
+
+    // Filter to change the 'Stock' header to 'Spaces' in the variant table
+add_filter('pvtfw_columns_labels', function($default) {
+    // Change the label for the stock column (availability_html)
+    if (isset($default['availability_html'])) {
+        $default['availability_html'] = __('Spaces', 'product-variant-table-for-woocommerce');
+    }
+    return $default;
+});
+
+// Customize the stock availability text to only show the stock quantity
+add_filter('woocommerce_get_availability_text', function($availability, $product) {
+    if ($product->is_in_stock()) {
+        $stock_quantity = $product->get_stock_quantity();
+        if ($stock_quantity > 0) {
+            $availability = sprintf(__('%d', 'woocommerce'), $stock_quantity); // Show only the quantity
+        }
+    }
+    return $availability;
+}, 10, 2);
+
+// Override stock availability display for out of stock products to show "0"
+add_filter('woocommerce_get_stock_html', function($html, $product) {
+    if (!$product->is_in_stock()) {
+        // If the product is out of stock, display "0" instead of "Out of stock"
+        $html = '<p class="stock out-of-stock">' . __('0', 'woocommerce') . '</p>';
+    }
+    return $html;
+}, 10, 2);
+
+
+// Redirect to /basket instead of /cart after adding product to cart
+add_filter('woocommerce_add_to_cart_redirect', function($url) {
+    // Set the URL to the /basket page
+    $basket_url = home_url('/basket');
+    
+    return $basket_url;
+});
