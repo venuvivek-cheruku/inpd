4ec25d16f998a24292ac5b705195312f6f3dcafe
venuvivek-cheruku <venuvivek.cheruku@outlook.com>
2024-10-09 00:30:28 +0100

    fixed cart

--- templates/woo-cart-template.php
+++ templates/woo-cart-template.php
@@ -43,16 +43,13 @@ do_action( 'woocommerce_before_cart' ); ?>
             <table class="shop_table shop_table_responsive cart woocommerce-cart-form__contents" cellspacing="0">
                 <thead>
                     <tr>
-                        <th class="product-remove"><span
-                                class="screen-reader-text"><?php esc_html_e( 'Remove item', 'woocommerce' ); ?></span>
-                        </th>
-                        <th class="product-thumbnail"><span
-                                class="screen-reader-text"><?php esc_html_e( 'Thumbnail image', 'woocommerce' ); ?></span>
-                        </th>
                         <th class="product-name"><?php esc_html_e( 'Product', 'woocommerce' ); ?></th>
                         <th class="product-price"><?php esc_html_e( 'Price', 'woocommerce' ); ?></th>
                         <th class="product-quantity"><?php esc_html_e( 'Quantity', 'woocommerce' ); ?></th>
                         <th class="product-subtotal"><?php esc_html_e( 'Subtotal', 'woocommerce' ); ?></th>
+                        <th class="product-remove"><span
+                                class="screen-reader-text"><?php esc_html_e( 'Remove item', 'woocommerce' ); ?></span>
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
@@ -78,34 +75,6 @@ do_action( 'woocommerce_before_cart' ); ?>
                     <tr
                         class="woocommerce-cart-form__cart-item <?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
 
-                        <td class="product-remove">
-                            <?php
-								echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-									'woocommerce_cart_item_remove_link',
-									sprintf(
-										'<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">&times;</a>',
-										esc_url( wc_get_cart_remove_url( $cart_item_key ) ),
-										/* translators: %s is the product name */
-										esc_attr( sprintf( __( 'Remove %s from cart', 'woocommerce' ), wp_strip_all_tags( $product_name ) ) ),
-										esc_attr( $product_id ),
-										esc_attr( $_product->get_sku() )
-									),
-									$cart_item_key
-								);
-							?>
-                        </td>
-
-                        <td class="product-thumbnail">
-                            <?php
-						$thumbnail = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
-
-						if ( ! $product_permalink ) {
-							echo $thumbnail; // PHPCS: XSS ok.
-						} else {
-							printf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $thumbnail ); // PHPCS: XSS ok.
-						}
-						?>
-                        </td>
 
                         <td class="product-name" data-title="<?php esc_attr_e( 'Product', 'woocommerce' ); ?>">
                             <?php
@@ -167,6 +136,23 @@ do_action( 'woocommerce_before_cart' ); ?>
                         <td class="product-subtotal" data-title="<?php esc_attr_e( 'Subtotal', 'woocommerce' ); ?>">
                             <?php
 								echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); // PHPCS: XSS ok.
+							?>
+                        </td>
+
+                        <td class="product-remove">
+                            <?php
+								echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+									'woocommerce_cart_item_remove_link',
+									sprintf(
+										'<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">&times;</a>',
+										esc_url( wc_get_cart_remove_url( $cart_item_key ) ),
+										/* translators: %s is the product name */
+										esc_attr( sprintf( __( 'Remove %s from cart', 'woocommerce' ), wp_strip_all_tags( $product_name ) ) ),
+										esc_attr( $product_id ),
+										esc_attr( $_product->get_sku() )
+									),
+									$cart_item_key
+								);
 							?>
                         </td>
                     </tr>
