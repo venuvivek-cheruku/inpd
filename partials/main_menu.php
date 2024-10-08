1602998bd3f976b00c8442d185a0104d9774a4a7
venuvivek-cheruku <venuvivek.cheruku@outlook.com>
2024-10-08 19:47:58 +0100

    added cart page

--- partials/main_menu.php
+++ partials/main_menu.php
@@ -36,7 +36,14 @@
                         </form>
                     </div>
                     <div class="basket">
-                        <a href="<?php echo wc_get_cart_url(); ?>">
+                        <a href="<?php echo wc_get_cart_url(); ?>" style="text-decoration: none;">
+                            <!-- Cart Item Count -->
+                            <?php $cart_count = WC()->cart->get_cart_contents_count(); ?>
+                            <?php if ( $cart_count > 0 ) : ?>
+                            <span class=" cart-count">
+                                <?php echo esc_html( $cart_count ); ?>
+                            </span>
+                            <?php endif; ?>
                             <svg id="Icon_ionic-ios-cart" data-name="Icon ionic-ios-cart"
                                 xmlns="http://www.w3.org/2000/svg" width="28.441" height="26.269"
                                 viewBox="0 0 28.441 26.269">
@@ -50,6 +57,7 @@
                                     d="M31.813,9.512a.41.41,0,0,0-.356-.309L9.182,6.889a.682.682,0,0,1-.513-.345,7.561,7.561,0,0,0-.834-1.367c-.527-.691-1.518-.669-3.338-.683A1.024,1.024,0,0,0,3.382,5.529,1.008,1.008,0,0,0,4.449,6.565a8.471,8.471,0,0,1,1.778.14c.321.1.581.669.677,1.161a.028.028,0,0,0,.007.022c.014.088.137.75.137.757L9.784,24.2a5.953,5.953,0,0,0,.992,2.624,2.668,2.668,0,0,0,2.25,1.191H29.207a1,1,0,0,0,.985-.985.994.994,0,0,0-.957-1.073H13.012a.758.758,0,0,1-.568-.206,3.4,3.4,0,0,1-.787-1.911l-.294-1.742a.04.04,0,0,1,.027-.044L30.383,18.6a.418.418,0,0,0,.335-.382L31.813,9.7A.45.45,0,0,0,31.813,9.512Z"
                                     transform="translate(-3.382 -4.493)" fill="#fff" />
                             </svg>
+
                         </a>
                     </div>
                     <div class="buttons">
@@ -115,18 +123,28 @@
                 </form>
             </div>
             <div class="basket">
-                <svg id="Icon_ionic-ios-cart" data-name="Icon ionic-ios-cart" xmlns="http://www.w3.org/2000/svg"
-                    width="28.441" height="26.269" viewBox="0 0 28.441 26.269">
-                    <path id="Path_1" data-name="Path 1"
-                        d="M11.624,29.156a1.031,1.031,0,1,1-1.031-1.031,1.031,1.031,0,0,1,1.031,1.031Z"
-                        transform="translate(-3.259 -3.917)" fill="#fff" />
-                    <path id="Path_2" data-name="Path 2"
-                        d="M27.409,29.156a1.031,1.031,0,1,1-1.031-1.031,1.031,1.031,0,0,1,1.031,1.031Z"
-                        transform="translate(-1.35 -3.917)" fill="#fff" />
-                    <path id="Path_3" data-name="Path 3"
-                        d="M31.813,9.512a.41.41,0,0,0-.356-.309L9.182,6.889a.682.682,0,0,1-.513-.345,7.561,7.561,0,0,0-.834-1.367c-.527-.691-1.518-.669-3.338-.683A1.024,1.024,0,0,0,3.382,5.529,1.008,1.008,0,0,0,4.449,6.565a8.471,8.471,0,0,1,1.778.14c.321.1.581.669.677,1.161a.028.028,0,0,0,.007.022c.014.088.137.75.137.757L9.784,24.2a5.953,5.953,0,0,0,.992,2.624,2.668,2.668,0,0,0,2.25,1.191H29.207a1,1,0,0,0,.985-.985.994.994,0,0,0-.957-1.073H13.012a.758.758,0,0,1-.568-.206,3.4,3.4,0,0,1-.787-1.911l-.294-1.742a.04.04,0,0,1,.027-.044L30.383,18.6a.418.418,0,0,0,.335-.382L31.813,9.7A.45.45,0,0,0,31.813,9.512Z"
-                        transform="translate(-3.382 -4.493)" fill="#fff" />
-                </svg>
+                <a href="<?php echo wc_get_cart_url(); ?>" style="text-decoration: none;">
+                    <!-- Cart Item Count -->
+                    <?php $cart_count = WC()->cart->get_cart_contents_count(); ?>
+                    <?php if ( $cart_count > 0 ) : ?>
+                    <span class=" cart-count" style="background-color: rgba(255, 255, 255, 0.35); color: #102E43;">
+                        <?php echo esc_html( $cart_count ); ?>
+                    </span>
+                    <?php endif; ?>
+                    <svg id="Icon_ionic-ios-cart" data-name="Icon ionic-ios-cart" xmlns="http://www.w3.org/2000/svg"
+                        width="28.441" height="26.269" viewBox="0 0 28.441 26.269">
+                        <path id="Path_1" data-name="Path 1"
+                            d="M11.624,29.156a1.031,1.031,0,1,1-1.031-1.031,1.031,1.031,0,0,1,1.031,1.031Z"
+                            transform="translate(-3.259 -3.917)" fill="#fff" />
+                        <path id="Path_2" data-name="Path 2"
+                            d="M27.409,29.156a1.031,1.031,0,1,1-1.031-1.031,1.031,1.031,0,0,1,1.031,1.031Z"
+                            transform="translate(-1.35 -3.917)" fill="#fff" />
+                        <path id="Path_3" data-name="Path 3"
+                            d="M31.813,9.512a.41.41,0,0,0-.356-.309L9.182,6.889a.682.682,0,0,1-.513-.345,7.561,7.561,0,0,0-.834-1.367c-.527-.691-1.518-.669-3.338-.683A1.024,1.024,0,0,0,3.382,5.529,1.008,1.008,0,0,0,4.449,6.565a8.471,8.471,0,0,1,1.778.14c.321.1.581.669.677,1.161a.028.028,0,0,0,.007.022c.014.088.137.75.137.757L9.784,24.2a5.953,5.953,0,0,0,.992,2.624,2.668,2.668,0,0,0,2.25,1.191H29.207a1,1,0,0,0,.985-.985.994.994,0,0,0-.957-1.073H13.012a.758.758,0,0,1-.568-.206,3.4,3.4,0,0,1-.787-1.911l-.294-1.742a.04.04,0,0,1,.027-.044L30.383,18.6a.418.418,0,0,0,.335-.382L31.813,9.7A.45.45,0,0,0,31.813,9.512Z"
+                            transform="translate(-3.382 -4.493)" fill="#fff" />
+                    </svg>
+
+                </a>
             </div>
             <button type='button' data-bs-toggle='collapse' data-bs-target='.navbar-collapse' id='nav-btn'
                 class='navbar-toggler ml-auto menu' aria-label='Menu Button'>
--- templates/course-archive.php
+++ templates/course-archive.php
@@ -3,117 +3,132 @@
     get_header();
 ?>
 
-    <section class="boxed-top-row" style="background: url('http://inpd.local:8888/wp-content/uploads/2024/07/inhousetrain.webp') no-repeat center center / cover;">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-lg-7">
-                    <div class="text-wrapper blue-box">
-                        <h1>Course Categories for Your Professional Development</h1>
-                        <p>We are dedicated to providing a comprehensive range of course categories that cover every aspect of corporate training. Our wide selection of course categories is thoughtfully designed to meet the unique requirements of businesses spanning across diverse sectors. </p>
-                    </div>
+<section class="boxed-top-row"
+    style="background: url('http://inpd.local:8888/wp-content/uploads/2024/07/inhousetrain.webp') no-repeat center center / cover;">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-7">
+                <div class="text-wrapper blue-box">
+                    <h1>Course Categories for Your Professional Development</h1>
+                    <p>We are dedicated to providing a comprehensive range of course categories that cover every aspect
+                        of corporate training. Our wide selection of course categories is thoughtfully designed to meet
+                        the unique requirements of businesses spanning across diverse sectors. </p>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
-    <section class="courses-arch">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="course-wrapper">
-                        <div class="single-course">
-                            <div class="top">
-                                <img src="/wp-content/uploads/2024/08/Mask-Group-27.png" alt="Card Header" class="img-fluid">
-                            </div>
-                            <div class="content">
-                                <p class="course-class">Directors</p>
-                                <h4>Directors Development Programme Accelerator</h4>
-                                <p>The programme is designed to support and provide leaders with the knowledge and skills to be a high performing…</p>
-                                <div class="button-wrapper mt-4">
-                                    <a href="#" class="siteCTA blue-border">Visit course</a>
-                                </div>
+<section class="courses-arch">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="course-wrapper">
+                    <div class="single-course">
+                        <div class="top">
+                            <img src="/wp-content/uploads/2024/08/Mask-Group-27.png" alt="Card Header"
+                                class="img-fluid">
+                        </div>
+                        <div class="content">
+                            <p class="course-class">Directors</p>
+                            <h4>Directors Development Programme Accelerator</h4>
+                            <p>The programme is designed to support and provide leaders with the knowledge and skills to
+                                be a high performing…</p>
+                            <div class="button-wrapper mt-4">
+                                <a href="#" class="siteCTA blue-border">Visit course</a>
                             </div>
                         </div>
-                        <div class="single-course">
-                            <div class="top">
-                                <img src="/wp-content/uploads/2024/08/Mask-Group-27.png" alt="Card Header" class="img-fluid">
-                            </div>
-                            <div class="content">
-                                <p class="course-class">Directors</p>
-                                <h4>Directors Development Programme Accelerator</h4>
-                                <p>The programme is designed to support and provide leaders with the knowledge and skills to be a high performing…</p>
-                                <div class="button-wrapper mt-4">
-                                    <a href="#" class="siteCTA blue-border">Visit course</a>
-                                </div>
+                    </div>
+                    <div class="single-course">
+                        <div class="top">
+                            <img src="/wp-content/uploads/2024/08/Mask-Group-27.png" alt="Card Header"
+                                class="img-fluid">
+                        </div>
+                        <div class="content">
+                            <p class="course-class">Directors</p>
+                            <h4>Directors Development Programme Accelerator</h4>
+                            <p>The programme is designed to support and provide leaders with the knowledge and skills to
+                                be a high performing…</p>
+                            <div class="button-wrapper mt-4">
+                                <a href="#" class="siteCTA blue-border">Visit course</a>
                             </div>
                         </div>
-                        <div class="single-course">
-                            <div class="top">
-                                <img src="/wp-content/uploads/2024/08/Mask-Group-27.png" alt="Card Header" class="img-fluid">
-                            </div>
-                            <div class="content">
-                                <p class="course-class">Directors</p>
-                                <h4>Directors Development Programme Accelerator</h4>
-                                <p>The programme is designed to support and provide leaders with the knowledge and skills to be a high performing…</p>
-                                <div class="button-wrapper mt-4">
-                                    <a href="#" class="siteCTA blue-border">Visit course</a>
-                                </div>
+                    </div>
+                    <div class="single-course">
+                        <div class="top">
+                            <img src="/wp-content/uploads/2024/08/Mask-Group-27.png" alt="Card Header"
+                                class="img-fluid">
+                        </div>
+                        <div class="content">
+                            <p class="course-class">Directors</p>
+                            <h4>Directors Development Programme Accelerator</h4>
+                            <p>The programme is designed to support and provide leaders with the knowledge and skills to
+                                be a high performing…</p>
+                            <div class="button-wrapper mt-4">
+                                <a href="#" class="siteCTA blue-border">Visit course</a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
-    <section class="c-arch video-with-bc grey-bg">
-        <div class="container">
-            <div class="row">
-                <div class="float-vid">
-                    <div class="video-img" data-aos="fade-right">
-                        <img src="http://inpd.local:8888/wp-content/uploads/2024/07/placeholder.jpg" alt="" class="img-fluid">
-                        <div class="vid-btn">
-                            <a data-bs-toggle="modal" data-bs-target=".vid-modal">
-                                <img src="/wp-content/uploads/2024/07/play-btn-1.svg" alt="Play Button" class="img-fluid">
-                            </a>
-                        </div>
+<section class="c-arch video-with-bc grey-bg">
+    <div class="container">
+        <div class="row">
+            <div class="float-vid">
+                <div class="video-img" data-aos="fade-right">
+                    <img src="http://inpd.local:8888/wp-content/uploads/2024/07/placeholder.jpg" alt=""
+                        class="img-fluid">
+                    <div class="vid-btn">
+                        <a data-bs-toggle="modal" data-bs-target=".vid-modal">
+                            <img src="/wp-content/uploads/2024/07/play-btn-1.svg" alt="Play Button" class="img-fluid">
+                        </a>
                     </div>
                 </div>
-                <div class="col-12 col-lg-7 offset-lg-5">
-                    <div class="text-wrapper">
-                        <h2>Find out more about our professional training courses</h2>
-                        <p>Whether you’re seeking to enhance your skills, explore new interests, or advance your career, our diverse range of courses are designed to meet your learning goals.</p>
+            </div>
+            <div class="col-12 col-lg-7 offset-lg-5">
+                <div class="text-wrapper">
+                    <h2>Find out more about our professional training courses</h2>
+                    <p>Whether you’re seeking to enhance your skills, explore new interests, or advance your career, our
+                        diverse range of courses are designed to meet your learning goals.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade vid-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered">
+            <div class="modal-content">
+                <div class="row">
+                    <div class="col-12 video-wrapper">
+                        <div class="close" data-bs-dismiss="modal">✕</div>
+                        <iframe src="https://www.youtube.com/embed/NpEaa2P7qZI?si=DaEx7BmFRlX0Rhgr" frameborder="0"
+                            allowfullscreen=""></iframe>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="modal fade vid-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-lg modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="row">
-                        <div class="col-12 video-wrapper">
-                            <div class="close" data-bs-dismiss="modal">✕</div>
-                            <iframe src="https://www.youtube.com/embed/NpEaa2P7qZI?si=DaEx7BmFRlX0Rhgr" frameborder="0" allowfullscreen=""></iframe>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    </div>
+</section>
 
-    <section class="course-lower-content grey-bg">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-lg-8 offset-lg-2">
-                    <div class="text-wrapper text-center mt-5">
-                        <p>Our expert tutors are dedicated to delivering engaging and interactive learning experiences. By blending theoretical knowledge with practical applications, we ensure that you gain the necessary expertise to thrive in your chosen area.</p>
-                        <div class="button-wrapper mt-4">
-                            <a href="#" class="siteCTA blue">Find out more</a>
-                        </div>
+<section class="course-lower-content grey-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-8 offset-lg-2">
+                <div class="text-wrapper text-center mt-5">
+                    <p>Our expert tutors are dedicated to delivering engaging and interactive learning experiences. By
+                        blending theoretical knowledge with practical applications, we ensure that you gain the
+                        necessary expertise to thrive in your chosen area.</p>
+                    <div class="button-wrapper mt-4">
+                        <a href="#" class="siteCTA blue">Find out more</a>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
 <?php get_footer(); ?>
